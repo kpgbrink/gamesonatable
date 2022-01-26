6780190b1ff3c203410e015ceb0a0a60db4eae53
kpgbrink <kpgbrink@gmail.com>
2022-01-25 21:57:33 -0500

    Remove userPhaser

--- packages/client/src/Components/Pages/PhaserPages/HostPage.tsx
+++ packages/client/src/Components/Pages/PhaserPages/HostPage.tsx
@@ -1,31 +1,33 @@
 import React, { useEffect } from "react";
 import Phaser from "phaser";
 import { useParams } from "react-router-dom";
-import socket from "../../../SocketConnection";
-import PhaserWrapper, { phaserDomId } from "./tools/PhaserWrapper";
 import PlayerStartingScene from "./PlayerScenes/PlayerStartingScene";
-import { usePhaser } from "./usePhaser";
+import PhaserWrapper from "./tools/PhaserWrapper";
+import socket from "../../../SocketConnection";
 
 export default function HostPage() {
   const { roomId, game } = useParams();
-  usePhaser({
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    scene: [PlayerStartingScene],
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 300 },
-        debug: false,
-      },
-    },
-    parent: phaserDomId,
-  });
 
   useEffect(() => {
     socket.emit("select game", game);
   }, [game]);
 
-  return <PhaserWrapper />;
+  return (
+    <PhaserWrapper
+      config={{
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        scene: [PlayerStartingScene],
+        physics: {
+          default: "arcade",
+          arcade: {
+            gravity: { y: 300 },
+            debug: false,
+          },
+        },
+      }}
+      gameName="Omaha"
+    />
+  );
 }
--- packages/client/src/Components/Pages/PhaserPages/tools/PhaserWrapper.tsx
+++ packages/client/src/Components/Pages/PhaserPages/tools/PhaserWrapper.tsx
@@ -1,5 +1,24 @@
-export const phaserDomId = `game`;
+import React from "react";
+import { useEffect } from "react";
+import Phaser from "phaser";
 
-export default function PhaserWrapper() {
-  return <div id={phaserDomId}></div>;
+type PhaserWrapperProps = {
+  config: Phaser.Types.Core.GameConfig;
+  gameName: string;
+};
+
+export default function PhaserWrapper({ config }: PhaserWrapperProps) {
+  const domId = `game`;
+
+  useEffect(() => {
+    const game = new Phaser.Game({
+      ...config,
+      parent: domId,
+    });
+    return () => {
+      game.destroy(true);
+    };
+  }, [config, domId]);
+
+  return <div id={domId}></div>;
 }
