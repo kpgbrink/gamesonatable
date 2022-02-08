572a66a69612d16974aaadef9de95f4a4e1484f5
kpgbrink <kpgbrink@gmail.com>
2022-02-07 22:40:04 -0500

    .

--- packages/client/src/PhaserPages/PlayerScenes/PlayerStartingScene.ts
+++ packages/client/src/PhaserPages/PlayerScenes/PlayerStartingScene.ts
@@ -1,7 +1,7 @@
 import { RoomData } from "api";
 import Phaser from "phaser";
 import socket from "../../SocketConnection";
-import { findMyUser } from "../tools/objects/tools";
+import { findMyUser } from "../tools/objects/Tools";
 import UserAvatarImage, { loadUserAvatarSprites } from "../tools/objects/UserAvatarSprite";
 import { onChangeGames } from "../tools/OnChangeGames";
 
