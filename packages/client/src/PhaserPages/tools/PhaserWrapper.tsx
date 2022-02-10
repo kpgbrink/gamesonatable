44086e575ed92dfda73c71695d59ec80c66966d7
kpgbrink <kpgbrink@gmail.com>
2022-02-09 23:27:10 -0500

    Auto resize window

--- packages/client/src/PhaserPages/tools/PhaserWrapper.tsx
+++ packages/client/src/PhaserPages/tools/PhaserWrapper.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { useEffect } from "react";
 import Phaser from "phaser";
+import React, { useEffect } from "react";
 
 type PhaserWrapperProps = {
   config: Phaser.Types.Core.GameConfig;
   gameName: string;
 };
+
+// add sizeChanged and game to window interface
+declare global {
+  interface Window {
+    sizeChanged: () => void;
+    game: Phaser.Game;
+  }
+}
 
 export default function PhaserWrapper({ config }: PhaserWrapperProps) {
   const domId = `game`;
@@ -14,6 +21,12 @@ export default function PhaserWrapper({ config }: PhaserWrapperProps) {
     const game = new Phaser.Game({
       ...config,
       parent: domId,
+      callbacks: {
+        postBoot: () => {
+          window.game = game;
+          window.sizeChanged();
+        },
+      },
     });
     return () => {
       game.destroy(true);
@@ -22,3 +35,16 @@ export default function PhaserWrapper({ config }: PhaserWrapperProps) {
 
   return <div id={domId}></div>;
 }
+
+window.sizeChanged = () => {
+  if (window.game.isBooted) {
+    setTimeout(() => {
+      window.game.scale.resize(window.innerWidth, window.innerHeight);
+      window.game.canvas.setAttribute(
+        "style",
+        `display: width: ${window.innerWidth}px; height: ${window.innerHeight}px;`
+      );
+    }, 100);
+  }
+};
+window.onresize = () => window.sizeChanged();
