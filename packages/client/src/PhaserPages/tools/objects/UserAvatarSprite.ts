80617bd66f21547cfeab289b1ee6e14239e70292
kpgbrink <kpgbrink@gmail.com>
2022-02-20 23:39:48 -0500

    random chance of not having an image.

--- packages/client/src/PhaserPages/tools/objects/UserAvatarSprite.ts
+++ packages/client/src/PhaserPages/tools/objects/UserAvatarSprite.ts
@@ -6,17 +6,24 @@ import { loadIfNotLoaded, randomIndex } from "./Tools";
 
 const playerFolder = 'assets/player/';
 
+// Random change of returning a -1 
+const randomNegativeOne = (negative1Chance: number) => {
+    return Math.random() < negative1Chance ? -1 : null;
+}
+
+
 export const generateRandomUserAvatar = (): UserAvatar => {
     const base = randomIndex(avatarImages.base);
-    const beard = randomIndex(avatarImages.beard);
-    const body = randomIndex(avatarImages.body);
-    const cloak = randomIndex(avatarImages.cloak);
-    const gloves = randomIndex(avatarImages.gloves);
-    const boots = randomIndex(avatarImages.boots);
-    const hair = randomIndex(avatarImages.hair);
-    const head = randomIndex(avatarImages.head);
-    const legs = randomIndex(avatarImages.legs);
+    const beard = randomNegativeOne(.8) || randomIndex(avatarImages.beard);
+    const body = randomNegativeOne(.01) || randomIndex(avatarImages.body);
+    const cloak = randomNegativeOne(.99) || randomIndex(avatarImages.cloak);
+    const gloves = randomNegativeOne(.60) || randomIndex(avatarImages.gloves);
+    const boots = randomNegativeOne(.05) || randomIndex(avatarImages.boots);
+    const hair = randomNegativeOne(.10) || randomIndex(avatarImages.hair);
+    const head = randomNegativeOne(.15) || randomIndex(avatarImages.head);
+    const legs = randomNegativeOne(.03) || randomIndex(avatarImages.legs);
     const userAvatar = { base, beard, body, cloak, gloves, boots, hair, head, legs };
+    console.log(userAvatar);
     socket.emit('set player avatar', userAvatar);
     return userAvatar;
 }
@@ -28,15 +35,30 @@ export const loadUserAvatarSprites = (scene: Phaser.Scene) => {
             const userAvatar = user.userAvatar;
             if (!userAvatar) return;
             loadIfNotLoaded(scene, `${userId}-base`, `${playerFolder}base/${avatarImages.base[userAvatar.base]}`);
-            loadIfNotLoaded(scene, `${userId}-beard`, `${playerFolder}beard/${avatarImages.beard[userAvatar.beard]}`);
-            loadIfNotLoaded(scene, `${userId}-body`, `${playerFolder}body/${avatarImages.body[userAvatar.body]}`);
-            loadIfNotLoaded(scene, `${userId}-cloak`, `${playerFolder}cloak/${avatarImages.cloak[userAvatar.cloak]}`);
-            loadIfNotLoaded(scene, `${userId}-gloves`, `${playerFolder}gloves/${avatarImages.gloves[userAvatar.gloves]}`);
-            loadIfNotLoaded(scene, `${userId}-boots`, `${playerFolder}boots/${avatarImages.boots[userAvatar.boots]}`);
-            loadIfNotLoaded(scene, `${userId}-hair`, `${playerFolder}hair/${avatarImages.hair[userAvatar.hair]}`);
-            loadIfNotLoaded(scene, `${userId}-head`, `${playerFolder}head/${avatarImages.head[userAvatar.head]}`);
-            loadIfNotLoaded(scene, `${userId}-legs`, `${playerFolder}legs/${avatarImages.legs[userAvatar.legs]}`);
-
+            if (userAvatar.cloak !== -1) {
+                loadIfNotLoaded(scene, `${userId}-cloak`, `${playerFolder}cloak/${avatarImages.cloak[userAvatar.cloak]}`);
+            }
+            if (userAvatar.gloves !== -1) {
+                loadIfNotLoaded(scene, `${userId}-gloves`, `${playerFolder}gloves/${avatarImages.gloves[userAvatar.gloves]}`);
+            }
+            if (userAvatar.body !== -1) {
+                loadIfNotLoaded(scene, `${userId}-body`, `${playerFolder}body/${avatarImages.body[userAvatar.body]}`);
+            }
+            if (userAvatar.beard !== -1) {
+                loadIfNotLoaded(scene, `${userId}-beard`, `${playerFolder}beard/${avatarImages.beard[userAvatar.beard]}`);
+            }
+            if (userAvatar.boots !== -1) {
+                loadIfNotLoaded(scene, `${userId}-boots`, `${playerFolder}boots/${avatarImages.boots[userAvatar.boots]}`);
+            }
+            if (userAvatar.hair !== -1) {
+                loadIfNotLoaded(scene, `${userId}-hair`, `${playerFolder}hair/${avatarImages.hair[userAvatar.hair]}`);
+            }
+            if (userAvatar.head !== -1) {
+                loadIfNotLoaded(scene, `${userId}-head`, `${playerFolder}head/${avatarImages.head[userAvatar.head]}`);
+            }
+            if (userAvatar.legs !== -1) {
+                loadIfNotLoaded(scene, `${userId}-legs`, `${playerFolder}legs/${avatarImages.legs[userAvatar.legs]}`);
+            }
             scene.load.start();
         });
     });
@@ -105,7 +127,6 @@ export default class UserAvatarImage extends Phaser.GameObjects.Container {
         if (this.legsImage) this.legsImage.destroy();
         this.legsImage = addImage(`${userId}-legs`);
         [this.cloakImage, this.baseImage, this.bodyImage, this.beardImage, this.glovesImage, this.bootsImage, this.hairImage, this.headImage, this.legsImage].reverse().forEach(image => {
-            console.log(image);
             if (!image) return;
             this.sendToBack(image);
         });
