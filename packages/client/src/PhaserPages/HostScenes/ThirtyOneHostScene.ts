856f348535c4c21c7bdb27de03f0816224d01cea
kpgbrink <kpgbrink@gmail.com>
2022-04-01 01:13:44 -0400

    Put user avatars in the game

--- packages/client/src/PhaserPages/HostScenes/ThirtyOneHostScene.ts
+++ packages/client/src/PhaserPages/HostScenes/ThirtyOneHostScene.ts
@@ -3,16 +3,13 @@ import GameTable from "../objects/GameTable";
 import { getScreenCenter, loadIfImageNotLoaded } from "../objects/Tools";
 import { ThirtyOneGame } from "./hostObjects/hostGame/ThirtyOneGame";
 import HostScene from "./hostObjects/HostScene";
-import { HostUserAvatarsAroundTableGame } from "./hostObjects/HostUserAvatars/HostUserAvatarsAroundTable/HostUserAvatarsAroundTableGame";
 
 export default class ThirtyOneHostScene extends HostScene {
     gameTable: GameTable | null = null;
-    hostUserAvatars: HostUserAvatarsAroundTableGame;
     thirtyOneGame: ThirtyOneGame;
 
     constructor() {
         super({ key: 'ThirtyOne' });
-        this.hostUserAvatars = new HostUserAvatarsAroundTableGame(this);
         this.thirtyOneGame = new ThirtyOneGame(this);
     }
 
@@ -24,9 +21,6 @@ export default class ThirtyOneHostScene extends HostScene {
 
     create() {
         super.create();
-        this.hostUserAvatars.createOnRoomData();
-        this.hostUserAvatars.moveToEdgeOfTable();
-
         const screenCenter = getScreenCenter(this);
         this.gameTable = new GameTable(this, screenCenter.x, screenCenter.y);
         this.gameTable.setDepth(2);
--- packages/client/src/PhaserPages/HostScenes/hostObjects/hostGame/HostCardGame.ts
+++ packages/client/src/PhaserPages/HostScenes/hostObjects/hostGame/HostCardGame.ts
@@ -1,21 +1,26 @@
 import { Cards } from "../../../objects/Cards";
 import { getScreenCenter } from "../../../objects/Tools";
 import { HostGame } from "../HostGame";
+import { HostUserAvatarsAroundTableGame } from "../HostUserAvatars/HostUserAvatarsAroundTable/HostUserAvatarsAroundTableGame";
 import { Shuffling } from "./states/hostCardGame/Shuffling";
 
 
 export class HostCardGame extends HostGame {
     cards: Cards
     scene: Phaser.Scene
+    hostUserAvatars: HostUserAvatarsAroundTableGame;
 
     constructor(scene: Phaser.Scene) {
         super(scene);
         this.scene = scene;
         this.cards = new Cards(scene);
+        this.hostUserAvatars = new HostUserAvatarsAroundTableGame(this.scene);
     }
 
     create() {
         super.create();
+        this.hostUserAvatars.createOnRoomData();
+        this.hostUserAvatars.moveToEdgeOfTable();
         const screenCenter = getScreenCenter(this.scene);
         this.cards.create(screenCenter.x, screenCenter.y);
         this.cards.setDepth(10);
