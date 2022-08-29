f696b333278bddefeb0e1cbc6ff06cc764a3521a
kpgbrink <kpgbrink@gmail.com>
2022-08-29 01:21:44 -0400

    Switch from State to Data

---
+++ packages/api/src/playerData/playerDatas/specificPlayerCardHandDatas/ThirtyOnePlayerCardHandData.ts
@@ -0,0 +1,9 @@
+import { PlayerCardHandData } from "../PlayerCardHandData";
+
+export class ThirtyOnePlayerCardHandData extends PlayerCardHandData {
+    canKnock: boolean = false;
+
+    canTakeCard: boolean = false;
+
+    canReturnCard: boolean = false;
+}
