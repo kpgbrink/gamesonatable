8328d7a3e9267e00c4fec0532d3cae0cb258cce0
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2023-09-03 20:39:35 -0700

    Remove unnecessary weird hacks intended for Heroku.

--- packages/client/build-api.js
+++
@@ -1,4 +0,0 @@
-const crossSpawn = require('cross-spawn');
-const { chdir, } = require('node:process');
-chdir('../api');
-process.exitCode = crossSpawn.sync('npm', ['run', 'build'], { stdio: 'inherit' }).status;
